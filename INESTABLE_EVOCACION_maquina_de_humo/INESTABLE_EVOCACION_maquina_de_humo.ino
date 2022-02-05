92c3bc60b0ea6cb58739c1f4e293c77c12eb03e9
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-02-04 21:20:39 -0600

    Codigo agregado

---
+++ INESTABLE_EVOCACION_maquina_de_humo/INESTABLE_EVOCACION_maquina_de_humo.ino
@@ -0,0 +1,37 @@
+//Configuración de pines
+int relay = 3; //En este pin va conectado el relay
+
+//Tiempos
+const double TIEMPO_ENCENDIDO = 15000;
+const double TIEMPO_APAGADO = 15000;
+const int CICLOS = 15; // Cuantas veces se activa en esa cantidad de segundos
+const double ARRANQUE_FRIO = 120000;
+
+const double DUTY_CYCLE = TIEMPO_ENCENDIDO / (CICLOS * 2);
+//Parte del programa que se ejecuta una vez
+//al encender el microcontrolador
+void setup() {
+  //Inicia la comunicación
+  Serial.begin (9600);
+  Serial.println ("Inicio");
+
+  //Se configura el pin y se inicia apagado
+  pinMode (relay, OUTPUT);
+  digitalWrite (relay, LOW);
+
+  delay (ARRANQUE_FRIO);
+}
+
+//Programa
+void loop() {
+  for (int i = 0; i < CICLOS; i++) {
+    Serial.println ("Encendido");
+    digitalWrite (relay, HIGH);
+    delay (DUTY_CYCLE);
+    Serial.println ("Apagado");
+    digitalWrite (relay, LOW);
+    delay (DUTY_CYCLE);
+  }
+  Serial.println ("Espera");
+  delay (TIEMPO_APAGADO);
+}
